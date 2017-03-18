d281fb7b7bd54eb56dff68b47f3569e13fe2e9ad
Fernando Balta <ferbj200@gmail.com>
2017-03-18 17:17:11 -0500

    actualizacion auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,21 +20,23 @@ Route::get('/', function () {
 Route::get('home', array('as' => 'home', 'uses' => function(){
   return view('home');
 }));
+Route::group(['middleware' => 'auth'], function() {
+		
+		Route::get('/get_event', function () {
+		    return view('create_initiative'); //TO-DO
+		});
 
+		Route::get('/get_manual_data', function () {
+			return view('get_manual_data');  //TO-DO
+		});
+
+		Route::get('/causaGetEvent/{id}', 'CausaController@getEvent');
+
+
+		Route::resource('causas', 'CausaController');
+
+		/*FACEBOOK*/
+		Route::get('auth/facebook', 'Auth\AuthController@redirectToProvider');
+		Route::get('auth/facebook/callback', 'Auth\AuthController@handleProviderCallback');
+});
 //Get data of the FB's Event
-Route::get('/get_event', function () {
-    return view('create_initiative'); //TO-DO
-});
-
-Route::get('/get_manual_data', function () {
-	return view('get_manual_data');  //TO-DO
-});
-
-Route::get('/causaGetEvent/{id}', 'CausaController@getEvent');
-
-
-Route::resource('causas', 'CausaController');
-
-/*FACEBOOK*/
-Route::get('auth/facebook', 'Auth\AuthController@redirectToProvider');
-Route::get('auth/facebook/callback', 'Auth\AuthController@handleProviderCallback');
