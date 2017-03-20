57cb974ec8be2f35e0834604895fd8b22d87a3da
Claudia Marquez <claudia92@gmail.com>
2017-03-20 02:14:55 -0500

    estilos header y footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,8 +15,15 @@
         <div class="container">
             <nav class="white row">
                 <div class="nav-wrapper col s12">
+                  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                   <a href="#" class="brand-logo"><img src="{{ url::asset('img/logo.png') }}"></a>
                   <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="http://emergenciaperu.com/reports">INCIDENCIAS</a></li>
+                    <li><a href="http://emergenciaperu.com/reports/submit">ENVIAR INCIDENCIA</a></li>
+                    <li><a href="http://emergenciaperu.com/alerts">RECIBIR ALERTAS</a></li>
+                    <li><a href="{{ url('/') }}" class="waves-effect waves-light btn grey-text"><img src="{{ url::asset('img/heart-icon.png') }}">BRINDAR AYUDA</a></li>
+                  </ul>
+                  <ul id="mobile-demo" class="close">
                     <li><a href="http://emergenciaperu.com/reports">INCIDENCIAS</a></li>
                     <li><a href="http://emergenciaperu.com/reports/submit">ENVIAR INCIDENCIA</a></li>
                     <li><a href="http://emergenciaperu.com/alerts">RECIBIR ALERTAS</a></li>
@@ -29,10 +36,19 @@
 
     @yield('content')
 
+    
+    <div class="footer row center">
+        <p>Está página ha sido hecha colaborativamente en Hackspace Perú. <a href="https://www.facebook.com/HackSpacePeru" target="_blank">CONTACTO</a></p>
+    </div>
+
 	<script type="text/javascript" src="{{URL::asset('js/helpers.js')}}"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="{{URL::asset('js/materialize.min.js')}}"></script>
-
+    <script type="text/javascript">
+        $(".button-collapse").click(function() {
+          $("#mobile-demo").slideToggle('100');
+        });
+    </script>
 	<!--Custom JS-->
 	@yield('js')
 	</body>
